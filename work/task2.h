224f82b9a10b75d848683d0eb663852ac358723f
Midory7 <33065952+Midory7@users.noreply.github.com>
2018-01-31 01:43:34 +0300

    Create task2.h

---
+++ work/task2.h
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void clearMatrix(char(*arr)[M]);
+void fillMatrix(char(*arr)[M]);
+void setMatrix(char(*arr)[M]);
+void printMatrix(char(*arr)[M]);
+void wait(int sec);
+void clrScr();
